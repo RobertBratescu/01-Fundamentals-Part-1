1dda948a91ba858c002f19cd800a2bca8603ef37
RobertBratescu <robert.bratescu29@gmail.com>
2021-10-29 01:51:44 +0300

    First commit

---
+++ starter/script.js
@@ -0,0 +1,57 @@
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Robert";
+// let PI = 3.1415;
+
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
+
+// console.log(myFirstJob);
+
+// let javascriptIsFun = true;
+
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof true);
+
+// javascriptIsFun = "YES";
+
+// lastName = "Bratescu";
+// console.log(lastName);
+// const now = 2037;
+// const ageRobert = now - 1993;
+// const ageSarah = now - 1990;
+
+// console.log(ageRobert, ageSarah);
+// console.log(ageRobert * 2, ageRobert ** 2);
+
+// let x, y;
+// x = y = 25 - 20;
+// console.log(x, y);
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+// let bmiMark = massMark / heightMark ** 2;
+// let bmiJohn = massJohn / heightJohn ** 2;
+
+// let markHigherBMI = bmiMark > bmiJohn;
+// console.log(bmiMark, bmiJohn, markHigherBMI);
+const firstName = "Robert";
+const myAge = 28;
+const myPosition = "Senior Marketing Specialist";
+const robertNew = `I'm ${firstName}, a ${myPosition}`;
+
+console.log(robertNew);
+console.log("String\n\
+with multiple\n\
+lines");
+console.log(`String
+with multiple
+lines`);
